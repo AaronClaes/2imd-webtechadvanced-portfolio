117c02a72134d4cbd6c68f1527264bc30c141ef6
Aaron Claes <aaronclaes22@gmail.com>
2021-03-14 20:47:52 +0100

    store values in localstorage + call api every hour

--- lab4 - app prototypes/app.js
+++ lab4 - app prototypes/app.js
@@ -7,9 +7,52 @@ class App {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lng = position.coords.longitude;
-      this.getWeather(lat, lng);
+      if (this.checkLocalStorage() === false) {
+        this.getWeather(lat, lng);
+      } else {
+        this.setData();
+      }
     });
   }
+
+  checkLocalStorage() {
+    let lastclear = localStorage.getItem("lastclear");
+    let now = new Date().getTime();
+    console.log(now - lastclear);
+    if (now - lastclear < 1000 * 60 * 60) {
+      if (
+        localStorage.getItem("temp") === null &&
+        localStorage.getItem("weatherId") === null
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      localStorage.clear();
+      return false;
+    }
+  }
+
+  // getKeys(key) {
+  //   let result;
+  //   return fetch("data.json")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((json) => {
+  //       switch (key) {
+  //         case "ytapikey":
+  //           result = json.YT_API_KEY;
+  //           console.log(result);
+  //           break;
+  //         case "weatherapikey":
+  //           result = json.WEATHER_API_KEY;
+  //           break;
+  //       }
+  //       return result;
+  //     });
+  // }
 
   getWeather(lat, lng) {
     fetch("data.json")
@@ -17,8 +60,8 @@ class App {
         return response.json();
       })
       .then((json) => {
-        let weatherapikey = json.WEATHER_API_KEY;
         let ytapikey = json.YT_API_KEY;
+        let weatherapikey = json.WEATHER_API_KEY;
         let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${weatherapikey}`;
 
         fetch(url)
@@ -27,7 +70,11 @@ class App {
           })
           .then((json) => {
             let temp = json.main.temp;
+            localStorage.setItem("temp", temp);
             let weatherId = json.weather[0].id;
+            console.log(weatherId);
+            localStorage.setItem("weatherId", weatherId);
+            localStorage.setItem("lastclear", new Date().getTime());
             let status = this.checkCondition(weatherId, temp);
             let query = this.getData(status);
             this.getSongs(query, ytapikey);
@@ -100,10 +147,9 @@ class App {
         subtitle = "Get summer vibes with";
         break;
     }
-
-    document.querySelector(".top").style.backgroundImage = `url(${background})`;
-    document.querySelector(".main__weather").innerHTML = title;
-    document.querySelector(".main__bottom__subtitle").innerHTML = subtitle;
+    background = localStorage.setItem("background", background);
+    title = localStorage.setItem("title", title);
+    subtitle = localStorage.setItem("subtitle", subtitle);
     return query;
   }
 
@@ -114,36 +160,59 @@ class App {
         return response.json();
       })
       .then((json) => {
-        let videoTitle = json.items[0].snippet.title; //videoTitle
-        let videoId = json.items[0].id.videoId; //videoID
+        let videoTitle = json.items[0].snippet.title;
+        localStorage.setItem("videoTitle", videoTitle);
+        let videoId = json.items[0].id.videoId;
+        localStorage.setItem("videoId", videoId);
         let channelName = json.items[0].snippet.channelTitle;
+        localStorage.setItem("channelName", channelName);
         let channelId = json.items[0].snippet.channelId;
 
-        document.querySelector(
-          ".footer__details__artist"
-        ).innerHTML = channelName;
-
-        document.querySelector(".footer__details__song").innerHTML = videoTitle;
-
-        let url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${ytapikey}`;
+        url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${ytapikey}`;
+        console.log(url);
         fetch(url)
           .then((response) => {
             return response.json();
           })
           .then((json) => {
             let channelIcon = json.items[0].snippet.thumbnails.high.url; //channelImg
-            document.querySelector(
-              ".footer__play-button-background"
-            ).style.backgroundImage = `url(${channelIcon})`;
-
-            document
-              .querySelector(".footer__play-button__link")
-              .setAttribute(
-                "href",
-                `https://music.youtube.com/watch?v=${videoId}`
-              );
+            localStorage.setItem("channelIcon", channelIcon);
+            return true;
+          })
+          .then((response) => {
+            if (response === true) {
+              this.setData();
+            }
           });
       });
+  }
+
+  setData() {
+    let videoTitle = localStorage.getItem("videoTitle");
+    let videoId = localStorage.getItem("videoId");
+    let channelName = localStorage.getItem("channelName");
+    let channelIcon = localStorage.getItem("channelIcon");
+    console.log(channelIcon);
+
+    let background = localStorage.getItem("background");
+    let title = localStorage.getItem("title");
+    let subtitle = localStorage.getItem("subtitle");
+
+    document.querySelector(".top").style.backgroundImage = `url(${background})`;
+    document.querySelector(".main__weather").innerHTML = title;
+    document.querySelector(".main__bottom__subtitle").innerHTML = subtitle;
+
+    document.querySelector(".footer__details__artist").innerHTML = channelName;
+
+    document.querySelector(".footer__details__song").innerHTML = videoTitle;
+
+    document.querySelector(
+      ".footer__play-button-background"
+    ).style.backgroundImage = `url(${channelIcon})`;
+
+    document
+      .querySelector(".footer__play-button__link")
+      .setAttribute("href", `https://music.youtube.com/watch?v=${videoId}`);
   }
 }
 
